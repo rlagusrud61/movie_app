3bdb9726ee88586e9a4de4b500128730f1cc348e
Hyeon Kyeong Kim <rlagusrud61@gmail.com>
2020-11-09 22:18:49 +0100

    Created Food class in App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
+function Food({ fav }) {
+  return <h1>I am a {fav}</h1>;
+}
 function App() {
-  return <div>Heyyyy</div>
+  return (
+    <div>
+      <h1>Herroooo</h1>
+      <Food fav="kimchi" />
+      <Food fav="라면" />
+      <Food fav="왕한국어도된다 개신기해" />
+      <Food fav="kimchi" />
+    </div>
+  );
 }
 
 export default App;
